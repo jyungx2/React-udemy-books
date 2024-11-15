52ad752eb08b41fec7ef8398fc468be9b2f7ce9a
jiyoungnim <cloundyon31@gmail.com>
2024-11-15 20:32:33 +0900

    📝 S7) ep.127 ~ 132: Creating/Fetching/Updating/Deleting a record with async/await network communication of REST client

--- src/App.js
+++ src/App.js
@@ -16,11 +16,34 @@ function App() {
   // Don't do this!
   // fetchBooks();
 
+  // 128. useEffect in action
+  // ❓1. Understanding when our arrow function gets called❓
+  // => useEffect로 보낸 함수는 컨텐츠와 돔이 최초로 업데이트(initial render) 된 후 무조건 호출되고, 그 다음부터는 2번째 매개변수에 따라 호출여부가 달라진다.
+  // 1) [] : called after 1st render / never called again.
+  // 2) - : called after 1st render / also called after every rerender.
+  // 3) [counter]: called after 1st render / also called after rerenders if 'counter' variable changed
+
+  // ❓2. Understanding the arrow function's return value❓
+
+  // ❓3. Understanding stale variable references❓
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   // 112. Updating the Title
-  const editBookById = (id, newTitle) => {
+  // ✅ server state(data)!
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    // 131. Thinking about updates(✅local state!)
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        // title: newTitle -> ...response.data
+        // * book: current book object(data)
+        // * response.data: updated book object that came back from an API server and might've been changed on other properties(title 이외의 다른 속성들이 바뀌면 그것또한 반영해야 하기 때문) => so we need to include this using spread operator as well.
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -28,7 +51,10 @@ function App() {
   };
 
   // 109. Deleting Records
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    // 132. Deleteing a record
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     // filter always returns a new array. don't have to use ... (spread operator)
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
@@ -43,6 +69,8 @@ function App() {
       title: title,
     });
 
+    // 125. Creating a new record
+    // {id: Math.round(Math.randon() * 9999), title} -> response.data (api 서버로부터 받아온 데이터 사용)
     const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
 
